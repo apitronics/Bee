04f53cdd8db8cff879e773321ce824c76bd6b2c2
Colin Dignam <colin.p.dignam@gmail.com>
2014-09-12 15:05:43 -0400

    initial - chmod all to 775

---
+++ sketches/libraries/BeeDevices/BeeDevices.h
@@ -0,0 +1,50 @@
+/*
+ * Apitronics - BeeDevices.h
+ * library for creating devices (sensors & actuators) for read/write commands from remote server
+ *
+ * Created: 9/12/2014
+ *  Author: Colin Dignam
+ *
+ */
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+  cout << "Hello World! ";
+ return 0;
+}
+
+#ifndef BeeDevices_H
+#define BeeDevices_H 
+//cout << "hello world!";
+
+class BeeDevice
+{
+public:
+	BeeDevice();
+	
+
+	
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*END BeeDevices_H */
+#endif
